cfbe136a868f194765c47b7924f1bb7694e56e6b
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2023-07-11 23:14:08 +0530

    defined a canvas and imported modules

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-import { Camera, Material, Texture } from "three/build/three.module.js";
+import { Camera, Group, Scene } from "three";
+
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
